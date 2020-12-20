bf9a490e797ca79c5613f5709e44597f9039ab73
Wallace Machado <64056460+WallaceMachado@users.noreply.github.com>
2020-12-20 16:30:31 -0300

    Tela de autenticação(login)

--- public/index.php
+++ public/index.php
@@ -2,12 +2,13 @@
 error_reporting(E_ERROR);  // mostrar somente erros e não advertencias
 require_once(dirname(__FILE__, 2) . '/src/config/config.php');// __FILE__ pega o caminho da pasta atual, o 2 sai da pasta atual e vai para a pasta pai e ai ele acessa '/src/config/config.php' e executa todo o código no arquivo
 
-require_once(dirname(__FILE__, 2) . '/src/models/User.php');
+require_once(dirname(__FILE__, 2) . '/src/views/login.php');
 //Database::getConnection();
 
+/*
 $user = new User(['name' => 'Lucas','email' => 'lucas@coder.com.br']);
 //echo $user->getSelect();
 print_r( User::get(['id'=>1],'name,email'));
 echo '<br>';
 print_r( User::get());
-//echo User::getSelect(['name'=>Chaves, 'email'=> 'chaves@cod3r.com.br']);
+//echo User::getSelect(['name'=>Chaves, 'email'=> 'chaves@cod3r.com.br']); */
