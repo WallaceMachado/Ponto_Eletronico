763980fd6f36027e198cd19d3764e2c35ee3aeac
Wallace Machado <64056460+WallaceMachado@users.noreply.github.com>
2020-12-20 12:15:27 -0300

    configuração de acesso banco de dados

--- public/index.php
+++ public/index.php
@@ -1,1 +1,5 @@
-<h1>ok</h1>
+<?php 
+error_reporting(E_ERROR);  // mostrar somente erros e não advertencias
+require_once(dirname(__FILE__, 2) . '/src/config/database.php');// __FILE__ pega o caminho da pasta atual, o 2 sai da pasta atual e vai para a pasta pai e ai ele acessa '/src/config/config.php'
+
+Database::getConnection();
---
+++ src/config/database.php
@@ -0,0 +1,35 @@
+<?php
+
+class Database {
+
+    public static function getConnection() {
+        $envPath = realpath(dirname(__FILE__) . '/../env.ini'); // __FILE__ pega o caminho da pasta atual, o ponto concatena e /../ sai da pasta atual para a anterior e ai ele acessa o arquivo env.ini
+        $env = parse_ini_file($envPath);
+        $conn = new mysqli($env['host'], $env['username'],
+            $env['password'], $env['database']); // pega os dados do arquivo, lembrando que o arquivo não será carregado no git pois está no arquivo git ignore
+        
+            // se tiver erro de conexão
+        if($conn->connect_error) {
+            die("Erro: " . $conn->connect_error);
+        }
+
+        return $conn;
+    }
+
+    public static function getResultFromQuery($sql) {
+        $conn = self::getConnection();
+        $result = $conn->query($sql);
+        $conn->close();
+        return $result;
+    }
+
+    public static function executeSQL($sql) {
+        $conn = self::getConnection();
+        if(!mysqli_query($conn, $sql)) {
+            throw new Exception(mysqli_error($conn));
+        }
+        $id = $conn->insert_id;
+        $conn->close();
+        return $id;
+    }
+}
