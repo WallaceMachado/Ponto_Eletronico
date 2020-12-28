e2e6e32859b979c1853d6f54655f33ac9e67d2a3
wallacemachado <wallacefm18@gmail.com>
2020-12-28 18:05:47 -0300

    Ativar regolio a ser atualizado automaticamente - parte JavaScript

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,4 +1,5 @@
-
+// codigo em java script, não precisa de ;
+//hard load na pagina para carregar o JS ctrl + shift + r
 (function () {
     const menuToggle = document.querySelector('.menu-toggle')
     menuToggle.onclick = function (e) {
@@ -6,3 +7,29 @@
         body.classList.toggle('hide-sidebar')
     }
 })()
+
+function ativarRelogio() {
+    const relogioAtivo = document.querySelector('[ative-relogio]')
+    if(!relogioAtivo) return // retorna nullo
+
+    function addUmSegundo(hours, minutes, seconds) {
+        const d = new Date()
+        d.setHours(parseInt(hours))
+        d.setMinutes(parseInt(minutes))
+        d.setSeconds(parseInt(seconds) + 1)
+    
+        const h = `${d.getHours()}`.padStart(2, 0)//padstart formata para numero de cartere que a variavel terá + o numero que será usado para completar caso não tenha 2
+        const m = `${d.getMinutes()}`.padStart(2, 0)
+        const s = `${d.getSeconds()}`.padStart(2, 0)
+    
+        return `${h}:${m}:${s}`
+    }
+
+    setInterval(function() {
+        // '07:27:19' => ['07', '27', '19']
+        const parts = relogioAtivo.innerHTML.split(':')//transforma uma straing em array conform acima
+        relogioAtivo.innerHTML = addUmSegundo(...parts)
+    }, 1000)
+}
+
+ativarRelogio()// chama a função a cima
