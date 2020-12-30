081302e133a3130bebb8b88cf141b912c9f59ef7
wallacemachado <wallacefm18@gmail.com>
2020-12-30 16:36:53 -0300

    listar usuarios

---
+++ src/controllers/users.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+requireValidSession();
+
+
+
+
+$users = User::get();
+foreach($users as $user) {
+    $user->start_date = (new DateTime($user->start_date))->format('d/m/Y');// trasforma o formatdo da data    
+    if($user->end_date) {
+        $user->end_date = (new DateTime($user->end_date))->format('d/m/Y');
+    }
+}
+
+loadTemplateView('users', [
+    'users' => $users,
+]);
